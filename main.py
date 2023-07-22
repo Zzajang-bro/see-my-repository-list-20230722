70cb3c2a73aa05f4c2bc75bc8efaf5de262a0ad7
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-07-22 09:48:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ with urllib.request.urlopen('https://api.github.com/users/zzajang-bro/repos') as
     content = response.read()
 dat = json.loads(content)
 #print(dat) // array of dict
-nameList = [(el['name'], el['description']) for el in dat]
-for i in range(len(nameList)):
-    print(nameList[i])
+nameList = [[el['name'], el['description']] for el in dat]
+print(json.dumps(nameList, ensure_ascii=False))
 input()
+# 참고: 2차원 JSON 표로 보기
+# https://github.com/Zzajang-bro/2d-json-viewer-20230722
